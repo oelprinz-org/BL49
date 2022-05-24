a234e2959ce76ad59c0a35808337295775fa2b09
HeinrichG-V12 <heinrich@bmw-rudel.de>
2022-05-24 19:35:25 +0200

    .gitignore added

--- software/BL49/BL49/Debug/sensor/sensor.d
+++
@@ -1,9 +0,0 @@
-sensor/sensor.d sensor/sensor.o: ../sensor/sensor.c ../sensor/sensor.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-../sensor/sensor.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
